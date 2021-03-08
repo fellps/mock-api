dab2d410f4aec695098f94915906b3cb9db3832d
Fellipe Vasconcelos <fsilva@appen.com>
2021-03-08 14:18:20 -0300

    feat: new endpoint

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const jsonServer = require('json-server');
-const cors = require('cors');
+const jsonServer = require("json-server");
+const cors = require("cors");
 
 const server = jsonServer.create();
-const router = jsonServer.router('src/db.json');
+const router = jsonServer.router("src/db.json");
 const middlewares = jsonServer.defaults();
 
-const PORT = 8080
+const PORT = 8080;
 
 server.use(
   cors({
     origin: true,
     credentials: true,
     preflightContinue: false,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   })
 );
-server.options('*', cors());
+server.options("*", cors());
 server.use(middlewares);
-server.use('/qrp/api/v2', router)
+server.use("/qrp/api/v2", router);
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
